ba84f221ab9a147468fac80bbab3402f5b802b90
bahmatov <bahmatov@fort-telecom.ru>
2024-06-16 22:36:41 +0500

    Закончил урок 13

--- news/models.py
+++ news/models.py
@@ -2,13 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Категории"
+        ordering = ['title']
+
+
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновленно')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True )
     is_published = models.BooleanField(default=True, verbose_name='Опубликованно')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
