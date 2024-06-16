de20325fa6444143077c31af3d8ccc7d70ee9f79
bahmatov <bahmatov@fort-telecom.ru>
2024-06-16 18:05:03 +0500

    Закончил урок 11

--- news/views.py
+++ news/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+from .models import News
 
 def index(request):
     # print(request)
-    return HttpResponse('Hello World')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news, 
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context)
 
 def test(request):
     return HttpResponse('<h1>Тестовая страница</h1>')
