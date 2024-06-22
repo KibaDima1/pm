696cc75c6651ebce2c4354706b558a09b3826d81
bahmatov <bahmatov@fort-telecom.ru>
2024-06-22 12:44:28 +0500

    Закончио урок 15 связанный с шаблонами

--- news/models.py
+++ news/models.py
@@ -22,6 +22,9 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Опубликованно')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+    def my_func(self):
+        return 'Hellow from model'
+    
     def __str__(self):
         return self.title
     
