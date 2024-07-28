39d295b0c596164c959b650ac534a8a4268db5d8
bahmatov <bahmatov@fort-telecom.ru>
2024-07-28 23:09:40 +0500

    Закончио урок 17 и 18

--- news/views.py
+++ news/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 def index(request):
     # print(request)
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news, 
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
